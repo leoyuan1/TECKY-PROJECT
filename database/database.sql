974c2dfbe605754318b34278aced044c1b9a66be
Leo Yuan <leo071525@gmail.com>
2023-01-17 14:35:12 +0800

    updated database

--- database/database.sql
+++ database/database.sql
@@ -10,28 +10,11 @@ CREATE TABLE messages (
     id SERIAL primary key,
     content TEXT,
     from_id INTEGER not null,
+    FOREIGN KEY(from_id) REFERENCES users(id),
     to_id INTEGER not null,
+    FOREIGN KEY(to_id) REFERENCES users(id),
     created_at TIMESTAMP not null,
     updated_at TIMESTAMP not null
-);
-CREATE TABLE posts (
-    pet_name TEXT,
-    species_id INTEGER,
-    gender TEXT,
-    birthday DATE,
-    user_id INTEGER,
-    description TEXT,
-    pet_type_id INTEGER,
-    status TEXT,
-    size TEXT,
-    created_at TIMESTAMP not null,
-    updated_at TIMESTAMP not null
-);
-CREATE TABLE post_media (
-    id SERIAL primary key,
-    file_name TEXT,
-    post_id INTEGER,
-    type TEXT
 );
 CREATE TABLE pet_types (
     id SERIAL primary key,
@@ -41,11 +24,37 @@ CREATE TABLE pet_types (
 );
 CREATE TABLE species (
     id SERIAL primary key,
-    pet_type_id TEXT,
+    pet_type_id INTEGER,
+    FOREIGN KEY(pet_type_id) REFERENCES pet_types(id),
     name TEXT
+);
+CREATE TABLE posts (
+    id SERIAL primary key,
+    FOREIGN KEY(id) REFERENCES users(id),
+    pet_name TEXT,
+    species_id INTEGER,
+    FOREIGN KEY(species_id) REFERENCES species(id),
+    gender TEXT,
+    birthday DATE,
+    user_id INTEGER,
+    description TEXT,
+    pet_type_id INTEGER,
+    FOREIGN KEY(pet_type_id) REFERENCES pet_types(id),
+    status TEXT,
+    size TEXT,
+    created_at TIMESTAMP not null,
+    updated_at TIMESTAMP not null
+);
+CREATE TABLE post_media (
+    id SERIAL primary key,
+    FOREIGN KEY(id) REFERENCES posts(id),
+    file_name TEXT,
+    post_id INTEGER,
+    type TEXT
 );
 CREATE TABLE post_comments (
     id SERIAL primary key,
+    FOREIGN KEY(id) REFERENCES posts(id),
     post_id INTEGER not null,
     created_at TIMESTAMP not null,
     created_by TIMESTAMP not null,
