fa1a4e4a92601e99c02e85f478a2f2fe4bb1a011
Jayden Choi <henrychoi1997@hotmail.com>
2023-01-18 17:45:49 +0800

    day update

--- server.ts
+++ server.ts
@@ -80,10 +80,10 @@ io.on("connection", function (socket) {
 app.use(express.static("public"));
 
 
-//  404
-app.use((req, res) => {
-    res.redirect('404.html')
-})
+// //  404
+// app.use((req, res) => {
+//     res.redirect('404.html')
+// })
 
 app.listen(8080, () => {
     console.log(`Listening at http://localhost:8080`);
