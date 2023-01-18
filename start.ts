43dbd9e117cd843871eadbc18bc104ef1930220e
Jayden Choi <henrychoi1997@hotmail.com>
2023-01-18 10:37:46 +0800

    html template

--- start.ts
+++ start.ts
@@ -1,8 +1,6 @@
-import express from "express";
 import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
-const app = express();
 
 const client = new pg.Client({
     database: process.env.DB_NAME,
@@ -45,19 +43,4 @@ async function main() {
 main();
 
 
-// app.get("/", function (req: Request, res: Response) {
-//     res.end("Hello World");
-// });
 
-// static files 
-app.use(express.static("public"));
-
-
-//  404
-app.use((req, res) => {
-    res.redirect('404.html')
-})
-
-app.listen(8080, () => {
-    console.log(`Listening at http://localhost:8080`);
-});
