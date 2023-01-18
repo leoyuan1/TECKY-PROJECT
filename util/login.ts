5d6055bb6abe4960c33f07493983a2ac465e9cdc
Leo Yuan <leo071525@gmail.com>
2023-01-18 17:38:14 +0800

    updated all file

--- util/login.ts
+++ util/login.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { client } from "./db";
-import { formParsePromise } from "./formidable";
+import { formidablePromise } from "./formidable";
+import { client } from "./psql-config";
 
 export const userRoutes = express.Router()
 userRoutes.post('/signup', signup)
 
 async function signup(req: express.Request, res: express.Response) {
-    let { fields, files } = await formParsePromise(req)
+    let { fields, files } = await formidablePromise(req)
     const getData = await client.query('select email, password from users')
 }
