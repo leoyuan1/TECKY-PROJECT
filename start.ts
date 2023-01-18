4dd94bab0a50eb8e85471e5dd89c7a6d8ee35b34
Jayden Choi <henrychoi1997@hotmail.com>
2023-01-18 11:11:02 +0800

    template updated

--- start.ts
+++ start.ts
@@ -63,19 +63,3 @@ async function main() {
 main();
 
 
-// app.get("/", function (req: Request, res: Response) {
-//     res.end("Hello World");
-// });
-
-// static files 
-app.use(express.static("public"));
-
-
-//  404
-app.use((req, res) => {
-    res.redirect('404.html')
-})
-
-app.listen(8080, () => {
-    console.log(`Listening at http://localhost:8080`);
-});
