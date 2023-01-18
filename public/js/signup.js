363382ec64a417d6d0da24046756805afd4a1b6a
Leo Yuan <leo071525@gmail.com>
2023-01-18 19:28:28 +0800

    updated signin

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,17 +1,27 @@
-const signupFormElm = document.querySelector('.signup-form')
 
-signupFormElm.addEventListener('submit', async (event) => {
-    event.preventDefault()
-    let formData = new FormData(signupFormElm)
+const signupFormElm = document.querySelector('.loginOrSignup-btn')
 
-    const res = await fetch('/signup', {
-        method: 'POST',
-        body: formData
+signupFormElm.addEventListener('click', async () => {
+    Swal.fire({
+        title: 'Login Form',
+        html: `<input type="text" id="login" class="swal2-input" placeholder="Email">
+        <input type="password" id="password" class="swal2-input" placeholder="Password">
+        `,
+        confirmButtonText: 'Sign in',
+        focusConfirm: false,
+        showCancelButton: true,
+        preConfirm: () => {
+            const login = Swal.getPopup().querySelector('#login').value
+            const password = Swal.getPopup().querySelector('#password').value
+            if (!login || !password) {
+                Swal.showValidationMessage(`Please enter login and password`)
+            }
+            return { login: login, password: password }
+        }
+    }).then((result) => {
+        Swal.fire(`
+        Login: ${result.value.login}
+        Password: ${result.value.password}
+        `.trim())
     })
-    if (res.ok) {
-        let data = await res.json()
-        signupFormElm.reset()
-    } else {
-        alert('fail')
-    }
 })
--- util/login.ts
+++ util/login.ts
@@ -7,5 +7,22 @@ userRoutes.post('/signup', signup)
 
 async function signup(req: express.Request, res: express.Response) {
     let { fields, files } = await formidablePromise(req)
-    const getData = await client.query('select email, password from users')
+    const getData = await client.query('select email from users where email = $1', [fields.email])
+    const foundUser = fields.email.row[0]
+    if (foundUser) {
+        res.status(402).json({
+            message: 'Invalid email'
+        })
+        return
+    }
+    const writeData = await client.query('INSERT INTO users (email,password,icon,username,created_at,updated_at) value ($1,$2,$3,$4,$5,now(),now())',
+        [fields.email,
+        fields.password,
+        files.icon,
+        fields.username,
+
+        ]
+
+    )
 }
+
