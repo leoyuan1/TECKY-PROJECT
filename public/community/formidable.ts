5db85fecdaae130efc6a15b48adcf0b8cf001551
Jayden Choi <henrychoi1997@hotmail.com>
2023-01-30 02:46:09 +0800

    logic update

--- public/community/formidable.ts
+++ public/community/formidable.ts
@@ -2,14 +2,14 @@ import formidable from "formidable";
 import express from "express";
 import fs from "fs";
 
-const uploadDir = "uploads/pet-img";
+const uploadDir = "uploads/post";
 fs.mkdirSync(uploadDir, { recursive: true });
 
 export const form = formidable({
     uploadDir,
     keepExtensions: true,
     maxFiles: 2,
-    maxFileSize: 50 * 1024 * 1024 ** 2, // the default limit is 50MB
+    maxFileSize: 50 * 1024 * 1024 ** 2, // the default  50MB
     filter: (part) => part.mimetype?.startsWith("image/") || part.mimetype?.startsWith("video/") || false,
     filename: (originalName, originalExt, part, form) => {
         let fieldName = part.name?.substring(0, part.name.length - 1);
