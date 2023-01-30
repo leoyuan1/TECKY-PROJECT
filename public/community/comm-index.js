dca914f12dc7fecba8eb82a6573c3a2bb973af07
Jayden Choi <henrychoi1997@hotmail.com>
2023-01-30 21:56:38 +0800

    ui final

--- public/community/comm-index.js
+++ public/community/comm-index.js
@@ -7,7 +7,7 @@ newPostformElm.addEventListener('click', async () => {
         title: '建立帖子',
         html: `<form id="form"><input type="text" id="title" class="swal2-input" name="email" placeholder="Title">
         <textarea type="text" id="content" class="swal2-input" name="text" placeholder="tpye here"></textarea> 
-        <button class="icon">Media upload<input type="file" name="image" id="image" /></button>
+        <button class="icon">Media upload<input type="file" name="image" id="media" /></button>
         </form>
         `,
 
@@ -18,14 +18,14 @@ newPostformElm.addEventListener('click', async () => {
         confirmButtonAriaLabel: 'Thumbs up, great!',
         preConfirm: async () => {
             const title = Swal.getPopup().querySelector('#title').value
-            const text = Swal.getPopup().querySelector('#text').value
-            const file = Swal.getPopup().querySelector('#file').value
+            const text = Swal.getPopup().querySelector('#content').value
+            const file = Swal.getPopup().querySelector('#media').value
             if (!title || !text) {
                 Swal.showValidatoinMessage(`Please fill the text area and title!`)
                 return
             }
             result = { title: title, text: text, file: file }
-            let res = await fetch('/index', {
+            let res = await fetch('/post', {
                 method: 'POST',
                 body: JSON.stringify(result),
                 headers: {
