080f0f32d64944c709a90a0d393c68065a1211b1
Jayden Choi <henrychoi1997@hotmail.com>
2023-02-01 19:30:36 +0800

    href update

--- database/initcommdata.ts
+++ database/initcommdata.ts
@@ -19,12 +19,12 @@ async function main() {
         title: "What is the best dogg on world?",
         content: "The best dog on world is husky!",
         media: "imag-1675219450191.jpeg",
-        id: "123",
+        id: "1",
         created_at: "2021-12-25 10:14:09.316",
         updated_at: "2021-12-25 10:14:09.316",
 
     };
-    const post1_id = (await client.query("Insert into community_messages(title, content, media, id, created_at, updated_at) values ($1,$2,$3,$4,$5,$6) returning id", [
+    const post1_id = (await client.query("Insert into community_messages(title, content, media, community_id, created_at, updated_at) values ($1,$2,$3,$4,$5,$6) returning id", [
         post1.title,
         post1.content,
         post1.id,
@@ -39,7 +39,7 @@ async function main() {
         post1_media_id: post1_id,
         post1_media_type: "image"
     }
-    await client.query("Insert into post1_media(media_name, media_id, media_type) values ($1, $2, $3", [
+    await client.query("Insert into post_media(media_name, media_id, media_type) values ($1, $2, $3", [
         post1_media.post1_media_name,
         post1_media.post1_media_id,
         post1_media.post1_media_type
